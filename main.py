cd3114af8b917a4c68b407993b078a511ba0bf4c
Marisa258 <167044704+Marisa258@users.noreply.github.com>
2024-04-23 22:29:17 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    while True:
+        s = input()
+
+        if s == 'END' :
+            break
+        else:
+            print("NONSENSE");
+
+
+
+if __name__ == "__main__":
+    main()
+
+        
